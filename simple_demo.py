a7c143b04ecded15818e50d5d809d7c822582551
Lines <linesbetween@users.noreply.github.com>
2017-02-26 07:32:55 -0500

    tyop

--- simple_demo.py
+++ simple_demo.py
@@ -64,7 +64,7 @@ def main():
     filemenu = Menu(menubar, tearoff=0)
     filemenu.add_command(label="Display Image", command=displayImg)
     filemenu.add_command(label="Dominant Color", command=analyzeImg)
-    filemenu.add_command(label="Play Music", command=palyMusic)
+    filemenu.add_command(label="Play Music", command=playMusic)
     menubar.add_cascade(label="File", menu=filemenu)
 
     root.config(menu=menubar)
